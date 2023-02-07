ae7bf796bf51019ed94d8ec4a8698057e44333c4
santoshsahu108 <sahu.santosh998@gmail.com>
2023-02-07 17:08:51 +0530

    second deploy

--- as.js
+++ as.js
@@ -1,17 +1,30 @@
 const express = require("express")
+
+const app = express()
+app.use(express.json())
+
+const fetch = require("node-fetch");
 const mongoose = require("mongoose")
-var ObjectId = require('mongodb').ObjectID;
 const Joi = require("joi")
 const nodemailer = require('nodemailer');
 const { response } = require("express");
-const app = express()
 var MongoClient = require('mongodb').MongoClient;
-app.use(express.json())
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(server);
+
+var err = {};
+let deptName
 let name
 let password
 let email
 let phone
 var url = "mongodb://localhost:27017/";
+const users = []
+
+
 
 
  
@@ -26,92 +39,132 @@ let mailTransporter = nodemailer.createTransport({
 
 
 DBcon()
-app.post('/createUser',async(req,res)=>{
+
+
+app.post('/createDept', async(req,res)=>{
+  const dept = new Dept({
+    deptName: req.body.deptName,
+
+  })
+  dept.save((err) => {
+    if (err) {
+      res.send({ message: err });
+           return;
+        }
+        else{
+          res.send({
+            message:
+              "Department was registered successfully",
+         });
+        }
+        res.end()
+      
+})
+
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+  app.post('/createUser',async (req,res)=>{
     //console.log("hellooo")
+    let present
     name = req.body.name
     password = req.body.password
     email = req.body.email
     phone = req.body.phone
+    deptName = req.body.deptName
     console.log(req.body, name,password,email,phone)
     
     const toValidate = {
       name : req.body.name,
       password : req.body.password,
       email : req.body.email,
-      phone : req.body.phone
+      phone : req.body.phone,
+      deptName : req.body.deptName
   }
+ 
     
   const validateObj =  validateUser(toValidate)
     
   if(validateObj.error)
   {  
-      //console.log(response.error.details)
-      res.send(validateObj.error.details)
+      console.log("errrrrrrrrrrr",JSON.stringify(validateObj.error.details[0]))
+
+    
+      res.send(JSON.stringify(validateObj.error.details[0]))
       res.end()
   }
   else
   {
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("Company");
-     
-      dbo.collection("emps").findOne({email : req.body.email}, function(err, out) {
-        if (err) throw err;
-        if(out){
+    const out = await Dept.findOne({deptName : req.body.deptName})
+    console.log("out>.........>>>>>",out)
+    //process.exit()
+
+    if (out){
+        present = out.deptName
+    
+
+     const out1 = await User.findOne({email : req.body.email})
+
+    if(out1){
           console.log("email is present");
           res.send({message : "email is taken"})
-        }else{
+         
+          
+    }else{
           const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
           let token = '';
           for (let i = 0; i < 25; i++) {
               token += characters[Math.floor(Math.random() * characters.length )];
           }
           
-              const user = new User({
+          const user = new User({
                 name: req.body.name,
                 email: req.body.email,
                 phone : req.body.phone,
                 password: req.body.password,
+                deptName : req.body.deptName,
                 confirmationCode: token
-              });
-              try{
+          });
+          try{
                 user.save((err) => {
                   if (err) {
                     res.send({ message: err });
                          return;
-                      }
-                      else{
-                        res.send({
+                  }
+                  else{
+
+                    res.send({
                           message:
                             "User was registered successfully! Please check your email",
-                       });
+                            deptName : out.deptName
+                    });
               
                        //mail( user.name,user.confirmationCode)
-                      }
-                      res.end()
-                    
-             });
+                  }
+                     
+                });
           
-              }catch (e) {
+          }
+          catch (e) {
                 res.status(400).send(e)
-            }
+              }
 
-        }
-        
-        db.close();
-      });
-    });
-   
-
-
-  }
-   
-
-
+          }
       
+    }
+    else{
+      res.send({deptName : "provide the proper department"})
+    }
+  }
+     
+})  
+   
+
 
   
-})
 
 app.patch('/updateUser', async(req,res)=>{
     console.log("name ->>>", req.query.id)
@@ -186,7 +239,6 @@ app.patch('/updateUser', async(req,res)=>{
 
         
 
-
 })
 app.delete('/deleteUser',async(req,res)=>{
   const Ido = req.query.id
@@ -215,13 +267,14 @@ app.delete('/deleteUser',async(req,res)=>{
 })
 app.get('/allUser',async (req,res)=>{
     User.find({}, function(err, users) {
-        var userMap = {};
+        // var userMap = {};
+        console.log("users in all users_-_",users)
     
-        users.forEach(function(user) {
-          userMap[user._id] = user;
-        });
+        // users.forEach(function(user) {
+        //   userMap[user._id] = user;
+        // });
     
-        res.send(userMap);  
+        res.send(users);  
       });
 })
 
@@ -295,8 +348,83 @@ app.get("/confirm/:confirmationCode",
       .catch((e) => console.log("error", e));
   })
 
+  io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+    socket.on('join', (department, callback) => {
+    // const { error, user } = addUser({ id: socket.id, ...options })
+    var myKey = "id";
+    var myValue = socket.id;
 
-app.listen(8000, () => console.log("listerning to port 8000"))
+    // Dynamically adding a key/value pair
+    department[myKey] = myValue;
+    console.log("depatment before pushing into array",department)
+      users.push(department)
+    // console.log("department---",department)
+    socket.join(department.deptName)
+
+    socket.emit('message', { message : `welcome ${department.name} to ${department.deptName} department`})
+    socket.broadcast.to(department.deptName).emit('message', { message : `new user - ${department.name}  added in ${department.deptName}  department ........`})
+    // io.to(department).emit('roomData', {
+    //     room: user.room,
+    //     users: getUsersInRoom(user.room)
+    // })
+
+    // callback()
+    })
+    socket.on('sendMessage', (message, callback) => {
+      console.log("_______socket----id-----",socket.id)
+      console.log("_______message----",message)
+      const user = getUser(socket.id)
+      console.log('useeeeeeeeeeeeer----', user)
+      console.log("))))))user.room---",user.deptName)
+
+      // io.to(user.room).emit('message', generateMessage(user.username, message))
+      io.to(user.deptName).emit('textMessage',`${user.name} : ${message}` )
+      callback()
+  })
+
+  })
+
+// io.sockets.on('connection', function(socket) {
+//   socket.on('create', function(room) {
+//     socket.join(room);
+//     console.log("joined to room _-----",room)
+//   });
+// });
+
+// io.on('connection', async (socket) => {
+
+//   console.log('a user connected');
+//   socket.on('chat message', (msg) => {
+//     console.log('message: ' +msg);
+//     fetch("http://localhost:8000/createUser", {
+//       method: 'POST',
+//       headers:{
+//           'Content-Type':'application/json'
+//       },
+//       body: msg
+//   })
+//   .then((res) => {console.log("response for fetch_-___-----",res)})
+//   .catch((err) => console.log("the error is ",err));
+    
+//   });
+//   // setInterval( async function(){
+//   var response =await fetch("http://localhost:8000/allUser")
+// 			var userData = await response.json()
+//       var sendData = JSON.stringify(Object.values(userData))
+//     console.log("response for fetch_-___-----",sendData)
+//     io.sockets.emit('broadcast',sendData);
+//   // },1000)
+
+  
+  
+ 
+
+  
+// });
+
+
+server.listen(8000, () => console.log("listerning to port 8000"))
 
 
 
@@ -317,6 +445,9 @@ db.once("open", function () {
  
 
 }
+var deptSchema = new mongoose.Schema({
+  deptName : String
+});
 var Schema = new mongoose.Schema({
     // _id    : String,
     name: String,
@@ -332,10 +463,12 @@ var Schema = new mongoose.Schema({
       },
       confirmationCode: { 
         type: String, 
-        unique: true }
+        unique: true },
+        deptName : String
+
 
     });
-     
+    var Dept  = mongoose.model('Dept', deptSchema);
     var User = mongoose.model('emp', Schema);
 
     function mail(name,confirmationCode){
@@ -386,6 +519,12 @@ var Schema = new mongoose.Schema({
                .max(10)
                .required(),
 
+        deptName : Joi.string()
+                .min(7)
+               .max(11)
+               .required(),
+
+
 
 }).options({ abortEarly: false });
 
@@ -424,19 +563,8 @@ return JoiSchema.validate(user)
 }
 
 
-async function validateID(o_id){
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("Company");
-    console.log("o_id--_________-------------",o_id)
-    dbo.collection("emps").findOne({"_id" : new ObjectId(o_id)}, function(err, result) {
-      if (err) throw err;
-      if (result){
-        return true
-      }else{
-        return false
-      }
-      db.close();
-    });
-  });
+const getUser = (id) => {
+
+  //console.log("after pushing into the array--", users)
+  return users.find((user) => user.id === id)
 }
